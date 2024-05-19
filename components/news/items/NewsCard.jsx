5806963e6012814be8f0db6a68663d82c8c6387a
SheikhFarid99 <sheikhfarid498@gmail.com>
2024-05-19 18:44:34 +0600

    update

--- components/news/items/NewsCard.jsx
+++ components/news/items/NewsCard.jsx
@@ -5,9 +5,9 @@ import Link from "next/link";
 const NewsCard = ({item}) => {
  
   return (
-    <div className="bg-white shadow flex gap-x-2 p-4">
+    <div className="bg-white shadow flex p-4">
       <div className="relative group overflow-hidden h-full">
-        <div className="group-hover:scale-[1.1] transition-all duration-[1s] w-[160px] h-[93px] lg:w-[100px] relative">
+        <div className="group-hover:scale-[1.1] transition-all duration-[1s] w-[100px] md:w-[160px] h-[93px] lg:w-[100px] relative">
           <Image
             className=""
             layout="fill"
@@ -19,7 +19,7 @@ const NewsCard = ({item}) => {
           <div className="w-full h-full block absolute left-0 top-0 invisible group-hover:visible bg-white cursor-pointer opacity-5 transition-all duration-300"></div>
         </div>
       </div>
-      <div className="flex flex-col gap-y-1">
+      <div className="flex flex-col gap-y-1 w-[calc(100%-100px)] md:w-[calc(100%-160px)] lg:w-[calc(100%-100px)] pl-3">
         <Link href={`/news/category/${ item?.category}`} className="text-sm font-semibold text-[#c80000]">
           {item?.category}
         </Link>
